aa5755cc5784827903750b9437369e19fd3bbee6
yash patel <21bmiit144@gmail.com>
2025-06-18 13:04:26 +0530

    changes

--- django_put_datas/django_mongo_datas/django_mongo_datas/settings.py
+++ django_put_datas/django_mongo_datas/django_mongo_datas/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_datas',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_mongo_datas.wsgi.application'
 
-
+ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY', 'default-encryption-key')
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
